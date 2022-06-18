52044a93afbe0d9e59173abc77b1eb32d193f043
Adriano Souza <adriano.msx8@gmail.com>
2022-06-18 16:41:57 -0300

    criação do metodo listar

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,86 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
+<!--<template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>-->
+
+<template>
+  <div id="app">
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Quiz</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <form>
+
+        <label>Nome</label>
+        <input type="text" placeholder="Nome">
+        <label>Quantidade</label>
+        <input type="number" placeholder="QTD">
+        <label>Valor</label>
+        <input type="text" placeholder="Valor">
+
+        <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+        <tr>
+          <th>NOME</th>
+          <th>QTD</th>
+          <th>VALOR</th>
+          <th>OPÇÕES</th>
+        </tr>
+
+        </thead>
+
+        <tbody>
+
+        <tr>
+
+          <td>Arduino</td>
+          <td>100</td>
+          <td>50.00</td>
+          <td>
+            <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+            <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+          </td>
+
+        </tr>
+
+        </tbody>
+
+      </table>
+
+    </div>
+
+  </div>
 </template>
 
+<script>
+
+import Pergunta from './services/perguntas'
+
+  export default {
+    mounted() {
+      Pergunta.listar().then(resposta => {
+        console.log(resposta)
+      })
+    }
+  }
+</script>
+
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,5 +91,5 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
